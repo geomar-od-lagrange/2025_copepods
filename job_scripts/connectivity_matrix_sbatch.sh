61e989efc7e5fa809c27737a74300ef89bae6cfa
JRethemeier <julian.rethemeier@posteo.de>
2026-06-24 14:48:01 +0200

    housekeeping files (deleting old files)

--- job_scripts/connectivity_matrix_sbatch.sh
+++ job_scripts/connectivity_matrix_sbatch.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH --job-name=Voronoi_connectivity
+#SBATCH --job-name=connectivity_original_locations
 #SBATCH --ntasks=4
 #SBATCH --cpus-per-task=2
 #SBATCH --mem-per-cpu=80G
-#SBATCH --time=24:00:00
+#SBATCH --time=4:00:00
 #SBATCH --partition=base
 
 
@@ -21,21 +21,20 @@ mkdir -p notebooks_executed/
 # run for single notebook and put into background
 # for year in {2024}; do
 year=2024
-for site in {16..27}; do
-        # mkdir -p output/VoronoiConnectivity/${year}/
-        mkdir -p notebooks_executed/VoronoiConnectivity/${year}/
+north_sea_locations='original'
+for site in {1..27}; do
+        mkdir -p notebooks_executed/connectivity_orignal/${year}/
         srun --ntasks=1 --nodes=1 --exclusive -c 2 singularity run -B /sfs -B /gxfs_work -B $PWD:/work --pwd /work parcels-container_2024.10.07-7af7fd0.sif bash -c \
         ". /opt/conda/etc/profile.d/conda.sh && conda activate base \
         && papermill --cwd notebooks/ \
                 notebooks/SiteConnectivityVoronoi.ipynb \
-                notebooks_executed/VoronoiConnectivity/${year}/Copepods_VoronoiConnectivity_${year}_s${site}.ipynb \
+                notebooks_executed/connectivity_orignal/${year}/connectivity_${north_sea_locations}_${year}_s${site}.ipynb \
                 -p site ${site} \
                 -p year ${year} \
+                -p north_sea_locations ${north_sea_locations} \
                 -p isPapermill True \
                 -k python" &
 done
-# done
-
     
 # wait till background task is done
 wait
